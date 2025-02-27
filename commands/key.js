a3279725d2cd221ac030f6e3ead561ca7ceda9d4
mnesh01 <97187030+mnesh01@users.noreply.github.com>
2025-02-27 23:20:08 +0300

    Enhance API key prompt with user input validation and messaging

--- commands/key.js
+++ commands/key.js
@@ -1,6 +1,24 @@
+const inquirer = require("inquirer").default;
+const colors = require("colors");
+const KeyManager = require('../lib/KeyManager');
+
+
 const key = {
-    set() {
-        console.log("Hello from set")
+    async set() {
+        const keyManager = new KeyManager();
+        const input = await inquirer.prompt([
+            {
+                type: "input",
+                name: "key",
+                message: "Enter the API key ".green + "https://coingecko.com"
+            }
+        ]);
+
+        const key = keyManager.setKey(input.key);
+
+        if (key) {
+            console.log("API key is set".blue);
+        }
     },
     show() {
         console.log("Hello from show")
