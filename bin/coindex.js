1630404cac6f32ea988d36b34e7b6ba331480e47
mnesh01 <97187030+mnesh01@users.noreply.github.com>
2025-02-28 15:39:57 +0300

    minor fixes

--- bin/coindex.js
+++ bin/coindex.js
@@ -7,5 +7,6 @@ const program = new Command();
 program 
     .version(pkg.version)
     .command('key', 'Manage API key --  https://coingecko.com')
+    .command('check', 'Check coin price')
     .parse(process.argv);
 
